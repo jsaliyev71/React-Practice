5f6bc35fff995cfd31594eae90aa213e357460c3
Jala Aliyeva <jsaliyev71@gmail.com>
2026-03-09 15:04:54 +0400

    Fix BrowserRouter basename trailing slash
    Testing

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import { Provider } from 'react-redux'
 import { store } from './redux/store/store.js'
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter basename="/React-Practice">
+  <BrowserRouter basename="/React-Practice/">
     <Provider store={store}>
       <ContextProvider>
           <App />
